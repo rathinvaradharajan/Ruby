6f6f8e1912075108834369413b8691940cf0f37a
root <root@201011JUNC0420.(none)>
2015-03-05 15:01:17 +0530

    added numbers,string concatination,input and output

---
+++ getname.rb
@@ -0,0 +1,11 @@
+first_name = "a"
+last_name = "b"
+color = "c"
+puts "Enter your name first name :"
+first_name = gets
+puts "Enter your last name :"
+last_name = gets
+puts "Your name is: "+first_name+ last_name
+puts "Enter your favourite color"
+color = gets
+puts "your favourite color is "+color
---
+++ name.rb
@@ -0,0 +1,9 @@
+first_name = "Rathin"
+last_name ="Varadharajan"
+color ="orange"
+puts first_name
+puts last_name
+puts color
+print "Name is " 
+puts first_name+" "+last_name
+puts first_name+" "+last_name+" "+color
